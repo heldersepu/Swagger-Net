94f8aa4f8d5a4146f8e60d4546d8d2b3ca298bbd
HelderSepu <HelderSepu@gmail.com>
2017-05-03 21:31:03 -0400

    Add UnitTest to reproduce issue #1058

---
+++ Swashbuckle.Dummy.Core/Controllers/TestBindingController.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Metadata;
+
+namespace Swashbuckle.Dummy.Controllers
+{
+    public class TestBindingController : ApiController
+    {
+        [Route("{id}")]
+        public string Get([Bind]string id)
+        {
+            return "abc";
+        }
+
+        [Route("NoBind")]
+        public string Get2()
+        {
+            return "abc";
+        }
+    }
+
+    public class BindAttribute : ParameterBindingAttribute
+    {
+        public override HttpParameterBinding GetBinding(HttpParameterDescriptor p)
+        {
+            return new FooBinding(p);
+        }
+    }
+    public class FooBinding : HttpParameterBinding
+    {
+        public FooBinding(HttpParameterDescriptor d) : base(d) { }
+
+        public override Task ExecuteBindingAsync(ModelMetadataProvider m, HttpActionContext a, CancellationToken c)
+        {
+            return Task.FromResult(true);
+        }
+    }
+}
