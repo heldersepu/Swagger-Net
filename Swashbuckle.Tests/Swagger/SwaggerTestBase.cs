2a1f4d4dbfee97217909822a72e228fc5c8dec66
HelderSepu <HelderSepu@gmail.com>
2017-06-23 15:22:53 -0400

    Add IncludeAllXmlComments on the SwaggerTestBase

--- Swashbuckle.Tests/Swagger/SwaggerTestBase.cs
+++ Swashbuckle.Tests/Swagger/SwaggerTestBase.cs
@@ -5,14 +5,13 @@ namespace Swashbuckle.Tests.Swagger
 {
     public class SwaggerTestBase : HttpMessageHandlerTestBase<SwaggerDocsHandler>
     {
-        protected SwaggerTestBase(string routeTemplate)
-            : base(routeTemplate)
-        {}
+        protected SwaggerTestBase(string routeTemplate) : base(routeTemplate) {}
 
         protected void SetUpHandler(Action<SwaggerDocsConfig> configure = null)
         {
             var swaggerDocsConfig = new SwaggerDocsConfig();
             swaggerDocsConfig.SingleApiVersion("v1", "Test API");
+            swaggerDocsConfig.IncludeAllXmlComments(null, AppDomain.CurrentDomain.BaseDirectory);
 
             configure?.Invoke(swaggerDocsConfig);
 
