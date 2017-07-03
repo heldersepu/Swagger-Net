6d8f8af42ae1aac77481703762a7db00e926b4ee
HelderSepu <HelderSepu@gmail.com>
2017-07-03 14:15:00 -0400

    Change the discoveryUrlSelector

--- Swashbuckle.Core/SwaggerUi/CustomAssets/discoveryUrlSelector.js
+++ Swashbuckle.Core/SwaggerUi/CustomAssets/discoveryUrlSelector.js
@@ -1,33 +1,12 @@
 ﻿(function () {
-    var DiscoveryUrlSelector = Backbone.View.extend({
-        render: function() {
-            // Don't re-render on subsequent reloads
-            var defaultVal = this.$el.val()
-            if (this.$el.prop('tagName') != 'SELECT') {
-                var select = $('<select id="input_baseUrl" name="baseUrl"></select>');
-                select
-                    .css('margin', '0')
-                    .css('border', '1px solid gray')
-                    .css('padding', '3px')
-                    .css('width', '400px')
-                    .css('font-size', '0.9em');
-
-                var rootUrl = this.options.rootUrl;
-                _.each(this.options.discoveryPaths, function(path) {
-                    var option = $('<option>' + rootUrl + "/" + path + '</option>');
-                    select.append(option);
-                });
-
-                select.val(defaultVal);
-                this.$el.replaceWith(select);
-            }
-            return this;
-        }
-    });
-
-    new DiscoveryUrlSelector({
-        el: $('#input_baseUrl'),
-        rootUrl: swashbuckleConfig.rootUrl,
-        discoveryPaths: swashbuckleConfig.discoveryPaths
-    }).render();
+    if ($('input.download-url-input=') != null) {
+        var url = swashbuckleConfig.rootUrl;
+        var style = 'style="width:100%;border:2px solid #547f00;font-weight:normal;font-family:Arial;"';
+        var select = '<select class="download-url-input" ' + style + ' >';
+        swashbuckleConfig.discoveryPaths.forEach(function (path) {
+            select += '<option>' + url + "/" + path + '</option>';
+        });
+        select += '</select>';
+        $('.download-url-input').outerHTML = select;
+    }
 })();
