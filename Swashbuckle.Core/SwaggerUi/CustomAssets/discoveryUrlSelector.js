b03b5dd3e07a577840ec470264601623c615e1cf
HelderSepu <heldersepu@gmail.com>
2017-07-03 19:49:14 -0400

    small correction to the selector

--- Swashbuckle.Core/SwaggerUi/CustomAssets/discoveryUrlSelector.js
+++ Swashbuckle.Core/SwaggerUi/CustomAssets/discoveryUrlSelector.js
@@ -1,5 +1,5 @@
 ﻿window.onload = function () {
-    if ($('input.download-url-input=') != null) {
+    if ($('input.download-url-input') != null) {
         var url = swashbuckleConfig.rootUrl;
         var style = 'style="width:100%;border:2px solid #547f00;font-weight:normal;font-family:Arial;"';
         var select = '<select class="download-url-input" ' + style + ' >';
