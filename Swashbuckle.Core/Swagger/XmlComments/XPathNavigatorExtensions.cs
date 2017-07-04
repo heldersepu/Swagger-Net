1eb114f9637ec7d9bc08c2c382668a587183ab35
HelderSepu <heldersepu@gmail.com>
2017-07-04 15:21:36 -0400

    UnitTest XPathNavigatorExtensions

--- Swashbuckle.Core/Swagger/XmlComments/XPathNavigatorExtensions.cs
+++ Swashbuckle.Core/Swagger/XmlComments/XPathNavigatorExtensions.cs
@@ -1,17 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Xml;
+﻿using System.Text.RegularExpressions;
 using System.Xml.XPath;
 
 namespace Swashbuckle.Swagger.XmlComments
 {
     public static class XPathNavigatorExtensions
     {
-        private static Regex ParamPattern = new Regex(@"<(see|paramref) (name|cref)=""([TPF]{1}:)?(?<display>.+?)"" />");
-        private static Regex ConstPattern = new Regex(@"<c>(?<display>.+?)</c>");
+        public static Regex ParamPattern = new Regex(@"<(see|paramref) (name|cref)=""([TPF]{1}:)?(?<display>.+?)"" />");
+        public static Regex ConstPattern = new Regex(@"<c>(?<display>.+?)</c>");
 
         public static string ExtractContent(this XPathNavigator node)
         {
@@ -20,18 +15,18 @@ namespace Swashbuckle.Swagger.XmlComments
             return XmlTextHelper.NormalizeIndentation(
                 ConstPattern.Replace(
                     ParamPattern.Replace(node.InnerXml, GetParamRefName),
-                GetConstRefName)
-                );
+                    GetConstRefName)
+            );
         }
 
-        private static string GetConstRefName(Match match)
+        public static string GetConstRefName(Match match)
         {
             if (match.Groups.Count != 2) return null;
 
             return match.Groups["display"].Value;
         }
 
-        private static string GetParamRefName(Match match)
+        public static string GetParamRefName(Match match)
         {
             if (match.Groups.Count != 5) return null;
 
---
+++ Tests/Swashbuckle.Tests/CoreUnitTests/XPathNavigatorExtensionsTests.cs
@@ -0,0 +1,71 @@
+﻿using NUnit.Framework;
+using Swashbuckle.Swagger.XmlComments;
+using System.IO;
+using System.Xml.XPath;
+
+namespace Swashbuckle.Tests.CoreUnitTests
+{
+    [TestFixture]
+    class XPathNavigatorExtensionsTests
+    {
+        [Test]
+        public void XPathNavigator_null()
+        {
+            XPathNavigator navigator = null;
+            string content = navigator.ExtractContent();
+            Assert.AreEqual(null, content);
+        }
+
+        [Test]
+        public void XPathNavigator_reads_empty_doc()
+        {
+            string xml = "<doc></doc>";
+            XPathDocument xmlDoc;
+            using (StringReader stream = new StringReader(xml))
+            {
+                xmlDoc = new XPathDocument(stream);
+            }
+            XPathNavigator navigator = xmlDoc.CreateNavigator();
+            string content = navigator.ExtractContent();
+            Assert.AreEqual(xml, content);
+        }
+
+        [Test]
+        public void XPathNavigator_reads_ConstPattern()
+        {
+            string xml = "<doc><c>test<display>one</display></c><c>test<display>two</display></c></doc>";
+            XPathDocument xmlDoc;
+            using (StringReader stream = new StringReader(xml))
+            {
+                xmlDoc = new XPathDocument(stream);
+            }
+            XPathNavigator navigator = xmlDoc.CreateNavigator();
+            string content = navigator.ExtractContent();
+            Assert.AreEqual(xml.Replace("<c>", "").Replace("</c>", ""), content.Strip());
+        }
+
+        [Test]
+        public void GetConstRefName_null()
+        {
+            string content = XPathNavigatorExtensions.ConstPattern.Replace("", XPathNavigatorExtensions.GetConstRefName);
+            Assert.IsEmpty(content);
+        }
+
+        [Test]
+        public void GetParamRefName_null()
+        {
+            string content = XPathNavigatorExtensions.ParamPattern.Replace("", XPathNavigatorExtensions.GetParamRefName);
+            Assert.IsEmpty(content);
+        }
+
+    }
+
+    internal static class StringExtensions
+    {
+        internal static string Strip(this string value)
+        {
+            if (string.IsNullOrEmpty(value)) return value;
+            return value.Replace("\n", "").Replace("\r", "").Replace(" ", "");
+        }
+    }
+}
