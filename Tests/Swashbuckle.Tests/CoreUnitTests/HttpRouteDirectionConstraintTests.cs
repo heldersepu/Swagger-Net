ec81aa6427b020c7fb9a2dc14280003c473e1d98
HelderSepu <heldersepu@gmail.com>
2017-07-04 16:27:55 -0400

    UnitTest HttpRouteDirectionConstraint

---
+++ Tests/Swashbuckle.Tests/CoreUnitTests/HttpRouteDirectionConstraintTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using Swashbuckle.Application;
+using Swashbuckle.Swagger.XmlComments;
+using System.IO;
+using System.Web.Http.Routing;
+using System.Xml.XPath;
+
+namespace Swashbuckle.Tests.CoreUnitTests
+{
+    [TestFixture]
+    class HttpRouteDirectionConstraintTests
+    {
+        [Test]
+        public void It_returns_match()
+        {
+            HttpRouteDirection direction =  HttpRouteDirection.UriResolution;
+            HttpRouteDirectionConstraint route = new HttpRouteDirectionConstraint(direction);
+            Assert.IsTrue(route.Match(null,null,"", null, direction));
+        }
+    }
+}
