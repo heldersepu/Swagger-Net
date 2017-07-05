c97e780752e38bdb3c5d79812324a71ca984e2a0
HelderSepu <heldersepu@gmail.com>
2017-07-04 22:43:42 -0400

    UnitTest ApplySwaggerOperationAttributes

---
+++ Tests/Swashbuckle.Tests/CoreUnitTests/ApplySwaggerOperationAttributesTests.cs
@@ -0,0 +1,41 @@
+﻿using Moq;
+using NUnit.Framework;
+using Swashbuckle.Swagger.Annotations;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http.Description;
+
+namespace Swashbuckle.Tests.CoreUnitTests
+{
+    [TestFixture]
+    class ApplySwaggerOperationAttributesTests
+    {
+        [Test]
+        public void GetAsset_null()
+        {
+            var collSwagger = new Collection<SwaggerOperationAttribute>()
+            {
+                new SwaggerOperationAttribute()
+                {
+                    OperationId = null,
+                    Tags = null,
+                    Schemes = null
+                }
+            };
+
+            var mock = new Mock<HttpActionDescriptor>();
+            mock.Setup(x => x.GetCustomAttributes<SwaggerOperationAttribute>()).Returns(collSwagger);
+
+            var apiDescription = new ApiDescription() { ActionDescriptor = mock.Object };
+            var op = new ApplySwaggerOperationAttributes();
+            Assert.DoesNotThrow(() => op.Apply(null, null, apiDescription));
+        }
+    }
+}
