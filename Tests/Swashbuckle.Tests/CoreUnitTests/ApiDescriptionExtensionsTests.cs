57a9d3e12b8c4b5d36688cfa028adde307d240b0
HelderSepu <HelderSepu@gmail.com>
2017-07-05 14:30:33 -0400

    UnitTest FriendlyId2

---
+++ Tests/Swashbuckle.Tests/CoreUnitTests/ApiDescriptionExtensionsTests.cs
@@ -0,0 +1,34 @@
+﻿using Moq;
+using NUnit.Framework;
+using Swashbuckle.Swagger;
+using System.Net.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Description;
+
+namespace Swashbuckle.Tests.CoreUnitTests
+{
+    [TestFixture]
+    class ApiDescriptionExtensionsTests
+    {
+        private ApiDescription apiDescription(string path, string method)
+        {
+            var mock = new Mock<HttpActionDescriptor>();
+            mock.Object.ControllerDescriptor = new HttpControllerDescriptor();
+
+            return new ApiDescription()
+            {
+                RelativePath = path,
+                HttpMethod = new HttpMethod(method),
+                ActionDescriptor = mock.Object
+            };
+        }
+
+        [TestCase("_PostAsdf", "asdf", "POST")]
+        [TestCase("_PostByAsdf", "{asdf", "POST")]
+        [TestCase("_PostByIdByCo", "{id}/{co}", "POST")]
+        public void FriendlyId2_Test(string expected, string path, string method)
+        {
+            Assert.AreEqual(expected, apiDescription(path, method).FriendlyId2());
+        }
+    }
+}
