204da6ba89ba77b958ba8e7df666a8723692a3f6
HelderSepu <heldersepu@gmail.com>
2017-07-05 20:29:40 -0400

    UnitTest ApplyXmlActionComments

---
+++ Tests/Swashbuckle.Tests/CoreUnitTests/ApplyXmlActionCommentsTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using Swashbuckle.Swagger.XmlComments;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace Swashbuckle.Tests.CoreUnitTests
+{
+    [TestFixture]
+    class ApplyXmlActionCommentsTests
+    {
+        [Test]
+        public void ApplyXmlActionComments_Empty()
+        {
+            Assert.Throws<ArgumentException>(() => new ApplyXmlActionComments(""));
+        }
+
+        [Test]
+        public void ApplyXmlActionComments_Test()
+        {
+            string directory = AppDomain.CurrentDomain.BaseDirectory;
+            string xmlFile = Directory.GetFiles(directory, "*.XML", SearchOption.AllDirectories).FirstOrDefault();
+            if (string.IsNullOrEmpty(xmlFile))
+            {
+                Assert.Inconclusive();
+            }
+            else
+            {
+                Assert.DoesNotThrow(() => new ApplyXmlActionComments(xmlFile));
+            }
+        }
+    }
+}
