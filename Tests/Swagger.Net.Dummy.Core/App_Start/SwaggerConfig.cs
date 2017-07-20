d0d492b57b4be87b8f2bd7f55120b7ef941a7617
HelderSepu <HelderSepu@gmail.com>
2017-07-20 11:15:51 -0400

    Add DocumentTitle to the  Dummy config

--- Tests/Swagger.Net.Dummy.Core/App_Start/SwaggerConfig.cs
+++ Tests/Swagger.Net.Dummy.Core/App_Start/SwaggerConfig.cs
@@ -195,6 +195,11 @@ namespace Swagger.Net.Dummy
                     })
                 .EnableSwaggerUi(c =>
                     {
+                        // Use the "DocumentTitle" option to change the Document title.
+                        // Very helpful when you have multiple Swagger pages open, to tell them apart.
+                        //
+                        c.DocumentTitle("Swagger.Net Dummy");
+
                         // Use the "InjectStylesheet" option to enrich the UI with one or more additional CSS stylesheets.
                         // The file must be included in your project as an "Embedded Resource", and then the resource's
                         // "Logical Name" is passed to the method as shown below.
