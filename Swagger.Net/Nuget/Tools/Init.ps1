adfac8fa30e9c3389b7566982d81a4699c54fcb8
HelderSepu <heldersepu@gmail.com>
2017-08-06 10:55:43 -0400

    Testing the powershell init

--- Swagger.Net/Nuget/Tools/Init.ps1
+++ Swagger.Net/Nuget/Tools/Init.ps1
@@ -1,7 +1,35 @@
 ﻿param($installPath, $toolsPath, $package, $project)
 
-$wshell = New-Object -ComObject Wscript.Shell
-$wshell.Popup($installPath,0,"Done",0x1)
-$wshell.Popup($toolsPath,0,"Done",0x1)
-$wshell.Popup($package,0,"Done",0x1)
-$wshell.Popup($project,0,"Done",0x1)
+Write-Host $installPath
+Write-Host $toolsPath
+Write-Host $package
+Write-Host $project
+
+$ProjectDir = "C:\Code\SwashbuckleTest\Swagger_Test\"
+$SwaggerConfigFile = Join-Path $ProjectDir "SwaggerConfig.cs"
+$AppStartDir = Join-Path $ProjectDir "App_Start\"
+Write-Host $AppStartDir
+
+$ValidPath = Test-Path $AppStartDir -IsValid
+If ($ValidPath -eq $True)
+{
+	$AppSwaggerConfig = Join-Path $AppStartDir "SwaggerConfig.cs"
+	$ValidFile = Test-Path $AppSwaggerConfig -IsValid
+	If ($ValidFile -eq $True)
+	{
+		Remove-Item $SwaggerConfigFile
+	}
+	Else
+	{
+		Move-Item $SwaggerConfigFile $AppStartDir
+	}
+}
+Else
+{
+	Remove-Item $SwaggerConfigFile
+}
+
+foreach ($item in $project.ProjectItems)
+{
+    Write-Host $item.Name
+}
