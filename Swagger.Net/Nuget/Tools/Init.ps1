69126ba63ee92632bdd131e0a92529d748cec832
HelderSepu <heldersepu@gmail.com>
2017-08-06 11:15:21 -0400

    Output the project folder

--- Swagger.Net/Nuget/Tools/Init.ps1
+++ Swagger.Net/Nuget/Tools/Init.ps1
@@ -1,35 +1,26 @@
 ﻿param($installPath, $toolsPath, $package, $project)
 
-Write-Host $installPath
-Write-Host $toolsPath
-Write-Host $package
-Write-Host $project
-
 $ProjectDir = "C:\Code\SwashbuckleTest\Swagger_Test\"
 $SwaggerConfigFile = Join-Path $ProjectDir "SwaggerConfig.cs"
 $AppStartDir = Join-Path $ProjectDir "App_Start\"
-Write-Host $AppStartDir
 
 $ValidPath = Test-Path $AppStartDir -IsValid
 If ($ValidPath -eq $True)
 {
-	$AppSwaggerConfig = Join-Path $AppStartDir "SwaggerConfig.cs"
-	$ValidFile = Test-Path $AppSwaggerConfig -IsValid
-	If ($ValidFile -eq $True)
-	{
-		Remove-Item $SwaggerConfigFile
-	}
-	Else
-	{
-		Move-Item $SwaggerConfigFile $AppStartDir
-	}
+    $AppSwaggerConfig = Join-Path $AppStartDir "SwaggerConfig.cs"
+    $ValidFile = Test-Path $AppSwaggerConfig -IsValid
+    If ($ValidFile -eq $True)
+    {
+        Remove-Item $SwaggerConfigFile
+    }
+    Else
+    {
+        Move-Item $SwaggerConfigFile $AppStartDir
+    }
 }
 Else
 {
-	Remove-Item $SwaggerConfigFile
+    Remove-Item $SwaggerConfigFile
 }
 
-foreach ($item in $project.ProjectItems)
-{
-    Write-Host $item.Name
-}
+Write-Host $project.ProjectItems(1).FileNames(1)
