0bf2735cd7b16d123e92d9496dc987029d56944a
HelderSepu <heldersepu@gmail.com>
2017-08-06 10:24:27 -0400

    Testing the params in the PS

--- Swagger.Net/Nuget/Tools/Init.ps1
+++ Swagger.Net/Nuget/Tools/Init.ps1
@@ -1,2 +1,7 @@
-﻿$wshell = New-Object -ComObject Wscript.Shell
-$wshell.Popup("Operation Completed",0,"Done",0x1)
+﻿param($installPath, $toolsPath, $package, $project)
+
+$wshell = New-Object -ComObject Wscript.Shell
+$wshell.Popup($installPath,0,"Done",0x1)
+$wshell.Popup($toolsPath,0,"Done",0x1)
+$wshell.Popup($package,0,"Done",0x1)
+$wshell.Popup($project,0,"Done",0x1)
