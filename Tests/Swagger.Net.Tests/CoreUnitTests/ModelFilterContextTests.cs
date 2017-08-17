fa511650bf14b84c57ff3fb0ffe359dbf1858aa7
HelderSepu <HelderSepu@gmail.com>
2017-08-17 11:25:46 -0400

    Add UnitTest for ModelFilterContextTests

---
+++ Tests/Swagger.Net.Tests/CoreUnitTests/ModelFilterContextTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace Swagger.Net.Tests.CoreUnitTests
+{
+    [TestFixture]
+    class ModelFilterContextTests
+    {
+        [Test]
+        public void SchemaRegistry_Null()
+        {
+            var x = new ModelFilterContext(null, null, null);
+            Assert.IsNull(x.SchemaRegistry);
+        }
+    }
+}
