f4b9e279e9a604b2a8a57995448a6ae3f6171b28
HelderSepu <HelderSepu@gmail.com>
2017-08-17 11:52:56 -0400

    Add JsonPropertyExtensionsTests

---
+++ Tests/Swagger.Net.Tests/CoreUnitTests/JsonPropertyExtensionsTests.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json.Serialization;
+using NUnit.Framework;
+using System.ComponentModel.DataAnnotations;
+
+namespace Swagger.Net.Tests.CoreUnitTests
+{
+    [TestFixture]
+    class JsonPropertyExtensionsTests
+    {
+        [Test]
+        public void FieldInfo_Null()
+        {
+            var x = new JsonProperty();
+            x.UnderlyingName = null;
+            Assert.IsNull(x.FieldInfo());
+        }
+
+        [Test]
+        public void FieldInfo_Test()
+        {
+            var x = new JsonProperty();
+            x.UnderlyingName = "test";
+            x.DeclaringType = typeof(Test);
+            Assert.IsNull(x.FieldInfo());
+        }
+    }
+
+    [MetadataType(typeof(int))]
+    public class Test
+    {
+        [MetadataType(typeof(int))]
+        public class Meta { }
+
+        public int id { get; set; }
+        public MetadataTypeAttribute attrib1 { get; set; }
+        public Meta attrib2 { get; set; }
+    }
+}
