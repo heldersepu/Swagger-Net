dc77d2e48ab1da4c713766366683623c0d46faeb
HelderSepu <HelderSepu@gmail.com>
2017-09-17 16:06:15 -0400

    Add UnitTest for Serializable

---
+++ Tests/Swagger.Net.Dummy.Core/Controllers/SerializableController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Web.Http;
+
+namespace Swagger.Net.Dummy.Controllers
+{
+    public class SerializableController : ApiController
+    {
+        public CountingLock Get(CountingLock cl)
+        {
+           return cl;
+        }
+    }
+
+    [Serializable]
+    public class CountingLock
+    {
+        public int EmployeeID { get; set; }
+        public string EmployeeName { get; set; }
+    }
+}
---
+++ Tests/Swagger.Net.Tests/Swagger/SerializableTests.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using Swagger.Net.Dummy.Controllers;
+
+
+namespace Swagger.Net.Tests.Swagger
+{
+    [TestFixture]
+    public class SerializableTests : SwaggerTestBase
+    {
+        public SerializableTests() : base("swagger/docs/{apiVersion}") { }
+
+        [SetUp]
+        public void SetUp()
+        {
+            SetUpDefaultRouteFor<SerializableController>();
+
+            // Default set-up
+            SetUpHandler();
+        }
+
+        [Test]
+        public void It_assigns_operation_properties_from_swagger_operation_attribute()
+        {
+            var swagger = GetContent<JObject>(TEMP_URI.DOCS);
+            var paths = swagger["paths"];
+            Assert.IsNotNull(paths);
+        }
+    }
+}
