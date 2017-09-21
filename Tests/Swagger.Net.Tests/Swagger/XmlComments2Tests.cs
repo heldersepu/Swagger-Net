5914235081eea310a90d0b2d8744bc4bc29082be
HelderSepu <HelderSepu@gmail.com>
2017-09-21 11:12:15 -0400

    Add UnitTest XmlComments2Tests

---
+++ Tests/Swagger.Net.Tests/Swagger/XmlComments2Tests.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using Swagger.Net.Dummy.Controllers;
+
+namespace Swagger.Net.Tests.Swagger
+{
+    [TestFixture]
+    public class XmlComments2Tests : SwaggerTestBase
+    {
+        public XmlComments2Tests() : base("swagger/docs/{apiVersion}") { }
+
+        [SetUp]
+        public void SetUp()
+        {
+            SetUpAttributeRoutesFrom(typeof(BlobController).Assembly);
+            SetUpDefaultRouteFor<BlobController>();
+            SetUpHandler();
+        }
+
+        [Test]
+        public void It_documents_operations_from_inherited_actions()
+        {
+            var swagger = GetContent<JObject>(TEMP_URI.DOCS);
+            Assert.IsNotNull(swagger);
+
+            var path = swagger["paths"]["/blob"];
+            Assert.IsNotNull(path);
+
+            Assert.IsNotNull(path["post"]["summary"]);
+            //Assert.IsNotNull(path["get"]["summary"]);
+        }
+    }
+}
