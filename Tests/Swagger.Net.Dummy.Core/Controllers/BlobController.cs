8ce31ac0a1febefcdca5b436189ad2e31f6eb0af
HelderSepu <HelderSepu@gmail.com>
2017-09-21 09:31:33 -0400

    Add BlobController

---
+++ Tests/Swagger.Net.Dummy.Core/Controllers/BlobController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Http;
+
+namespace Swagger.Net.Dummy.Controllers
+{
+    public abstract class Blob<T> : ApiController
+    {
+        /// <summary>GET</summary>
+        public int Get(int? x) { return 0; }
+
+        /// <summary>POST</summary>
+        public int Post(int x) { return 0; }
+    }
+
+    public class Foo { }
+
+    public class BlobController : Blob<Foo> { }
+}
