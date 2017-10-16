f17312454f9b99aae9c8c1d379615bba8b16a5a0
HelderSepu <HelderSepu@gmail.com>
2017-10-16 15:26:56 -0400

    Add Put with AnotherFoo to BlobController

--- Tests/Swagger.Net.Dummy.Core/Controllers/BlobController.cs
+++ Tests/Swagger.Net.Dummy.Core/Controllers/BlobController.cs
@@ -9,9 +9,14 @@ namespace Swagger.Net.Dummy.Controllers
 
         /// <summary>POST</summary>
         public int Post(int x) { return 0; }
+
+        /// <summary>PUT</summary>
+        public int Put(AnotherFoo<T> x) { return 0; }
     }
 
     public class Foo { }
+
+    public class AnotherFoo<TV> { }
 
     public class BlobController : Blob<Foo> { }
 }
