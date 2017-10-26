e31ee634cb182bff49f752382664d405e60f7a96
HelderSepu <HelderSepu@gmail.com>
2017-10-26 14:04:43 -0400

    Enable the SetValidatorUrl

--- Tests/Swagger.Net.Dummy.Core/App_Start/SwaggerConfig.cs
+++ Tests/Swagger.Net.Dummy.Core/App_Start/SwaggerConfig.cs
@@ -218,7 +218,7 @@ namespace Swagger.Net.Dummy
                         // By default, swagger-ui will validate specs against swagger.io's online validator and display the result
                         // in a badge at the bottom of the page. Use these options to set a different validator URL or to disable the
                         // feature entirely.
-                        //c.SetValidatorUrl("http://localhost/validator");
+                        c.SetValidatorUrl("https://online.swagger.io/validator");
                         //c.DisableValidator();
 
                         // Use this option to control how the Operation listing is displayed.
