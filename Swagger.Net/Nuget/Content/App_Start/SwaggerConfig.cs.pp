73d11ba92211e6b49bab29fd0f78fe30c88e007a
HelderSepu <HelderSepu@gmail.com>
2017-12-05 13:48:32 -0500

    Add ShowExtensions to SwaggerConfig

--- Swagger.Net/Nuget/Content/App_Start/SwaggerConfig.cs.pp
+++ Swagger.Net/Nuget/Content/App_Start/SwaggerConfig.cs.pp
@@ -213,6 +213,11 @@ namespace $rootnamespace$
                         // for example 0 and 1.
                         //
                         //c.BooleanValues(new[] { "0", "1" });
+						
+                        // Controls the display of vendor extension (x-) fields and values for Operations, Parameters, and Schema. 
+                        // The default is false.
+                        //
+                        //c.ShowExtensions(false);
 
                         // By default, swagger-ui will validate specs against swagger.io's online validator and display the result
                         // in a badge at the bottom of the page. Use these options to set a different validator URL or to disable the
