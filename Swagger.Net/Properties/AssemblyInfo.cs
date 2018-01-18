ef8a5719ea693ccacbd1694c1191182789544dc1
HelderSepu <HelderSepu@gmail.com>
2018-01-18 07:56:26 -0500

    Revert "Match all Assembly*Version"
    This reverts commit a9016768a672c8786b345a15ec8774d88c89fb95.

--- Swagger.Net/Properties/AssemblyInfo.cs
+++ Swagger.Net/Properties/AssemblyInfo.cs
@@ -11,7 +11,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("6a5ef762-7172-4bfb-98d2-076c3987fb7c")]
-
-[assembly: AssemblyVersion("8.3.9.103")]
-[assembly: AssemblyFileVersion("8.3.9.103")]
-[assembly: AssemblyInformationalVersion("8.3.9.103")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("8.3.9.102")]
