c0a188fee2c66a2ec49cab63616455a8e9d680d5
HelderSepu <HelderSepu@gmail.com>
2018-01-23 08:57:34 -0500

    Inc Version

--- Swagger.Net/Properties/AssemblyInfo.cs
+++ Swagger.Net/Properties/AssemblyInfo.cs
@@ -1,17 +1,23 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Swagger.Net.Core")]
+[assembly: AssemblyTitle(Assemb.Title)]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Swagger.Net.Core")]
-[assembly: AssemblyCopyright("CopyLeft ©  2017")]
+[assembly: AssemblyProduct(Assemb.Title)]
+[assembly: AssemblyCopyright("CopyLeft ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("6a5ef762-7172-4bfb-98d2-076c3987fb7c")]
 
-[assembly: AssemblyVersion("8.3.9.103")]
-[assembly: AssemblyFileVersion("8.3.9.103")]
-[assembly: AssemblyInformationalVersion("8.3.9.103")]
+[assembly: AssemblyVersion(Assemb.Version)]
+[assembly: AssemblyFileVersion(Assemb.Version)]
+[assembly: AssemblyInformationalVersion(Assemb.Version)]
+
+internal struct Assemb
+{
+    public const string Title = @"Swagger.Net.Core";
+    public const string Version = @"8.3.9.201";
+}
