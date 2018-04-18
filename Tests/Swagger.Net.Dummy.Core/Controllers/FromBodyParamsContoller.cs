989d958317aec6286bc249704512ef5bf7398ba8
Helder Sepulveda <heldersepu@gmail.com>
2018-04-18 11:22:29 -0400

    Add FromBodyParamsController

---
+++ Tests/Swagger.Net.Dummy.Core/Controllers/FromBodyParamsContoller.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+
+namespace Swagger.Net.Dummy.Controllers
+{
+    public class FromBodyParamsController : ApiController
+    {
+        [HttpGet]
+        public Transaction Get([FromBody] Transaction t)
+        {
+            return t;
+        }
+    }
+}
