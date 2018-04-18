c4105889754600c13e2dbc17599bf315468dc4f9
Helder Sepulveda <heldersepu@gmail.com>
2018-04-18 12:36:40 -0400

    Correction for the filenames

---
+++ Tests/Swagger.Net.Dummy.Core/Controllers/FromHeaderParamsController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+
+namespace Swagger.Net.Dummy.Controllers
+{
+    public class FromHeaderParamsController : ApiController
+    {
+        [HttpGet]
+        public string Get([FromBody] string t)
+        {
+            return t;
+        }
+    }
+}
