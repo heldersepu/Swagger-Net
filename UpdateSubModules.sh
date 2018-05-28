71fda9c315031cd2b790ee3c58f8ad5a64546865
Helder Sepulveda <heldersepu@gmail.com>
2018-05-28 15:32:15 -0400

    Add script to update submodules

---
+++ UpdateSubModules.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# Update all SubModules to latest and commit
+
+cd swagger-ui
+git checkout master
+git pull 
+cd ..
+
+cd swagger-ui-themes
+git checkout develop
+git pull 
+cd ..
+
+git commit -am "Update Swagger-UI"
