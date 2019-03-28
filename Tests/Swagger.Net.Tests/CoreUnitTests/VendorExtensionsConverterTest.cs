961ef521af2dd1c910f89a773035da2e203ab54b
Helder Sepulveda <heldersepu@gmail.com>
2019-03-27 21:44:54 -0400

    Add test on VendorExtensionsConverter.ReadJson

---
+++ Tests/Swagger.Net.Tests/CoreUnitTests/VendorExtensionsConverterTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using Swagger.Net.Application;
+using System;
+
+namespace Swagger.Net.Tests.CoreUnitTests
+{
+    [TestFixture]
+    class VendorExtensionsConverterTests
+    {
+        [Test]
+        public void ReadJson_Test()
+        {
+            var obj = new VendorExtensionsConverter();
+            Assert.Throws<NotImplementedException>(() => obj.ReadJson(null, null, null, null));
+        }
+    }
+}
