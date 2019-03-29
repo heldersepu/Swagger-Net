02ef8d8cb5760e249420ca7f2b39e8a5efc1ba5b
Helder Sepulveda <heldersepu@gmail.com>
2019-03-28 22:19:26 -0400

    One more check on IfModifiedSince

--- Tests/Swagger.Net.Tests/Swagger/CoreTests.cs
+++ Tests/Swagger.Net.Tests/Swagger/CoreTests.cs
@@ -677,6 +677,8 @@ namespace Swagger.Net.Tests.Swagger
 
             var swagger = Get(TEMP_URI.DOCS);
             Assert.AreEqual(HttpStatusCode.OK, swagger.StatusCode);
+            swagger = Get(TEMP_URI.DOCS, new DateTimeOffset(DateTime.Now.AddDays(-1)));
+            Assert.AreEqual(HttpStatusCode.OK, swagger.StatusCode);
             swagger = Get(TEMP_URI.DOCS, new DateTimeOffset(DateTime.Now.AddDays(1)));
             Assert.AreEqual(HttpStatusCode.NotModified, swagger.StatusCode);
         }
