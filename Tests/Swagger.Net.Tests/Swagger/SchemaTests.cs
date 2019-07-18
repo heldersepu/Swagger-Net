a2089267a408c69963d3ae67fde999af650e2758
Helder Sepulveda <heldersepu@gmail.com>
2019-07-17 23:02:39 -0400

    Move  SchemaTestsWithCustomAuthorizeAttribute to own file

--- Tests/Swagger.Net.Tests/Swagger/SchemaTests.cs
+++ Tests/Swagger.Net.Tests/Swagger/SchemaTests.cs
@@ -1110,33 +1110,4 @@ namespace Swagger.Net.Tests.Swagger
             Assert.IsNotNull(swagger.definitions["ComplexObject2"].required);
         }
     }
-
-    [TestFixture]
-    public class SchemaTestsWithCustomAuthorizeAttribute : SwaggerTestBase
-    {
-        public SchemaTestsWithCustomAuthorizeAttribute()
-            : base("swagger/docs/{apiVersion}")
-        {
-        }
-
-        [Test]
-        public void It_adds_security_to_protected_operations()
-        {
-            SetUpDefaultRouteFor<ProtectedResourcesController>();
-            SetUpDefaultRouteFor<ProtectedWithCustomAttributeResourcesController>();
-
-            SetUpHandler(c =>
-            {
-                c.ApiKey("apiKey", "header", "API Key Authentication");
-            });
-
-            var swagger = GetContent<SwaggerDocument>(TEMP_URI.DOCS);
-
-            var paths = swagger.paths.ToArray();
-
-            Assert.IsNotNull(paths[0].Value.get.security);
-            Assert.IsNotNull(paths[1].Value.get.security);
-
-        }
-    }
 }
---
+++ Tests/Swagger.Net.Tests/Swagger/SchemaTestsWithCustomAuthorizeAttribute.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using Swagger.Net.Dummy.Controllers;
+using System.Linq;
+
+namespace Swagger.Net.Tests.Swagger
+{
+    [TestFixture]
+    public class SchemaTestsWithCustomAuthorizeAttribute : SwaggerTestBase
+    {
+        public SchemaTestsWithCustomAuthorizeAttribute() : base("swagger/docs/{apiVersion}") {}
+
+        [Test]
+        public void It_adds_security_to_protected_operations()
+        {
+            SetUpDefaultRouteFor<ProtectedResourcesController>();
+            SetUpDefaultRouteFor<ProtectedWithCustomAttributeResourcesController>();
+
+            SetUpHandler(c =>
+            {
+                c.ApiKey("apiKey", "header", "API Key Authentication");
+            });
+
+            var swagger = GetContent<SwaggerDocument>(TEMP_URI.DOCS);
+
+            var paths = swagger.paths.ToArray();
+
+            Assert.IsNotNull(paths[0].Value.get.security);
+            Assert.IsNotNull(paths[1].Value.get.security);
+        }
+    }
+}
