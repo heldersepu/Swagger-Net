85d777c1a01e7693230a6576272b814a5f183d19
Helder Sepulveda <heldersepu@gmail.com>
2019-07-17 23:27:47 -0400

    Add test on ApiKey

---
+++ Tests/Swagger.Net.Dummy.Core/Controllers/ProtectedResources2Controller.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Web.Http;
+
+namespace Swagger.Net.Dummy.Controllers
+{
+    public class ProtectedResources2Controller : ApiController
+    {
+        [TokenAuth]
+        public string Get()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class TokenAuthAttribute : Attribute
+    {
+    }
+}
---
+++ Tests/Swagger.Net.Tests/Swagger/SecurityTests2.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using Swagger.Net.Dummy.Controllers;
+using Swagger.Net.Tests.Swagger;
+
+namespace Swagger.Net.Tests.SwaggerFilters
+{
+    [TestFixture]
+    public class SecurityTests2 : SwaggerTestBase
+    {
+        public SecurityTests2() : base("swagger/docs/{apiVersion}") { }
+
+        [SetUp]
+        public void SetUp()
+        {
+            SetUpDefaultRouteFor<ProtectedResources2Controller>();
+        }
+
+        [Test]
+        public void It_exposes_securityDefinitions()
+        {
+            SetUpHandler(c =>
+            {
+                c.ApiKey("token", "header", "TokenAuth", typeof(TokenAuthAttribute));
+            });
+
+            var swagger = GetContent<JObject>(TEMP_URI.DOCS);
+            var securityDefinitions = swagger["securityDefinitions"];
+            var expected = JObject.FromObject(new
+            {
+                token = new
+                {
+                    type = "apiKey",
+                    description = "TokenAuth",
+                    name = "token",
+                    @in = "header",
+                },
+            });
+
+            Assert.AreEqual(expected.ToString(), securityDefinitions.ToString());
+        }
+    }
+}
