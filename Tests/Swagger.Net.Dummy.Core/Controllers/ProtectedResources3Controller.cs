b0ca1f6239369c6ac2905d56a48d43cb62ae56b5
Helder Sepulveda <heldersepu@gmail.com>
2019-08-25 06:58:33 -0400

    Add dummy TokenClassAuth

---
+++ Tests/Swagger.Net.Dummy.Core/Controllers/ProtectedResources3Controller.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web.Http;
+
+namespace Swagger.Net.Dummy.Controllers
+{
+    [TokenClassAuth]
+    public class ProtectedResources3Controller : ApiController
+    {
+        public string Get()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Class)]
+    public class TokenClassAuthAttribute : Attribute
+    {
+    }
+}
