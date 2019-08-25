0f8cf8a4a6c195c1a250da0d3414e3c149af6218
Helder Sepulveda <heldersepu@gmail.com>
2019-08-25 06:58:52 -0400

    Add test on TokenClassAuth

---
+++ Tests/Swagger.Net.Tests/Swagger/SecurityTests3.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using Swagger.Net.Dummy.Controllers;
+using Swagger.Net.Tests.Swagger;
+
+namespace Swagger.Net.Tests.SwaggerFilters
+{
+    [TestFixture]
+    public class SecurityTests3 : SwaggerTestBase
+    {
+        public SecurityTests3() : base("swagger/docs/{apiVersion}") { }
+
+        [SetUp]
+        public void SetUp()
+        {
+            SetUpDefaultRouteFor<ProtectedResources3Controller>();
+        }
+
+        [Test]
+        public void It_exposes_securityDefinitions()
+        {
+            SetUpHandler(c =>
+            {
+                c.ApiKey("token", "header", "TokenAuth", typeof(TokenClassAuthAttribute));
+            });
+
+            var swagger = GetContent<JObject>(TEMP_URI.DOCS);
+            var securityDefinitions = swagger["securityDefinitions"];
+            var expected = JObject.FromObject(new
+            {
+                token = new
+                {
+                    type = "apiKey",
+                    description = "TokenAuth",
+                    name = "token",
+                    @in = "header",
+                },
+            });
+
+            Assert.AreEqual(expected.ToString(), securityDefinitions.ToString());
+        }
+
+        [Test]
+        public void It_exposes_security_on_action()
+        {
+            SetUpHandler(c =>
+            {
+                c.ApiKey("token", "header", "TokenAuth", typeof(TokenClassAuthAttribute));
+            });
+
+            var swagger = GetContent<SwaggerDocument>(TEMP_URI.DOCS);
+
+            Assert.IsNotNull(swagger.paths["/protectedresources3"].get.security);
+        }
+    }
+}
