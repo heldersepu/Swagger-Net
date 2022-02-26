511627d6f10d1f7315a543f454cb368cccd7c71d
Helder Sepulveda <heldersepu@gmail.com>
2022-02-26 18:34:39 -0500

    Update Swagger-UI

--- Swagger.Net/Properties/AssemblyInfo.cs
+++ Swagger.Net/Properties/AssemblyInfo.cs
@@ -19,5 +19,5 @@ using System.Runtime.InteropServices;
 public struct SwaggerAssemb
 {
     public const string Title = @"Swagger.Net.Core";
-    public const string Version = @"8.4.1.301";
+    public const string Version = @"8.4.5.201";
 }
