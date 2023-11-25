82fa5098fae21a352fc1d55099f8c9b8b953e372
Helder Sepulveda <heldersepu@gmail.com>
2023-11-24 21:13:48 -0500

    Inc version

--- Swagger.Net/Properties/AssemblyInfo.cs
+++ Swagger.Net/Properties/AssemblyInfo.cs
@@ -19,5 +19,5 @@ using System.Runtime.InteropServices;
 public struct SwaggerAssemb
 {
     public const string Title = @"Swagger.Net.Core";
-    public const string Version = @"8.4.19.001";
+    public const string Version = @"8.5.10.301";
 }
